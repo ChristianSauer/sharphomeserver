da0686ff14f3d9073ed1bf1e7eacbc242f1abb0b
ChristianSauer <christian.sauer@email.de>
2020-10-23 14:58:47 +0200

    working navigation

---
+++ SharpHomeServer/Data/Reading.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SharpHomeServer
+{
+    public class Reading    {
+        public string reading_type { get; set; }
+        public double value { get; set; }
+        public DateTime date { get; set; }
+    }
+
+}
---
+++ SharpHomeServer/RavenDbDocumentStore.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Options;
+using Raven.Client.Documents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace SharpHomeServer
+{
+    public class RavenDbDocumentStore : IRavenDbDocumentStore
+    {
+
+        public RavenDbDocumentStore(IOptions<RavenDbOptions> options)
+        {
+            ravenDbOptions = options.Value;
+            store = new Lazy<IDocumentStore>(CreateStore, System.Threading.LazyThreadSafetyMode.ExecutionAndPublication);
+        }
+
+        private readonly RavenDbOptions ravenDbOptions;
+
+        // Use Lazy<IDocumentStore> to initialize the document store lazily. 
+        // This ensures that it is created only once - when first accessing the public `Store` property.
+        private readonly Lazy<IDocumentStore> store;
+
+        public IDocumentStore Store => store.Value;
+
+        private IDocumentStore CreateStore()
+        {
+            IDocumentStore store = new DocumentStore()
+            {
+                // Define the cluster node URLs (required)
+                Urls = ravenDbOptions.Urls,
+
+                // Define a default database (optional)
+                Database = ravenDbOptions.Database,
+
+                // Define a client certificate (optional)
+                Certificate = new X509Certificate2(ravenDbOptions.CertificatePath, "phi1plun-FEA5lont"),
+
+                // Initialize the Document Store
+            }.Initialize();
+
+            return store;
+        }
+    }
+}
